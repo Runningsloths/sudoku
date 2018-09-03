ebb83eee51072ffae117cd92b3186f82b60e4b98
Runningsloths <32084733+Runningsloths@users.noreply.github.com>
2018-09-03 01:43:09 -0700

    Add files via upload

--- GameDriver.java
+++ GameDriver.java
@@ -24,5 +24,6 @@ public class GameDriver
                 state.setBoard(row, col, val);
             }
         }
+        System.out.println(state);
     }
 }
--- SudokuHandler.java
+++ SudokuHandler.java
@@ -11,6 +11,8 @@ public class SudokuHandler implements GameState
     }
     public void genBoard()
     {
+    //takes too long
+
         List<Integer> vals = Arrays.asList(1,2,3,4,5,6,7,8,9);
         int counter = 1;
         Collections.shuffle(vals);
@@ -18,6 +20,10 @@ public class SudokuHandler implements GameState
             board[0][i] = vals.get(i);
         }
         while (counter < BOARD_SIZE) {
+            boolean hasZeros = false;
+            for (int i = 0; i < BOARD_SIZE; i++) {
+                board[counter][i] = 0;
+            }
             Collections.shuffle(vals);
             for (int i = 0; i < BOARD_SIZE; i++) {
                 if (conditionsMet(counter, i, vals.get(i))) {
@@ -27,14 +33,72 @@ public class SudokuHandler implements GameState
                     break;
                 }
             }
-            if (IntStream.of(board[counter]).anyMatch(x -> x != 0)) {
+            for (int i : board[counter]) {
+                if (i == 0) {
+                    hasZeros = true;
+                }
+            }
+            if (!hasZeros) {
                 counter++;
+            }
+            System.out.println(toString());
+            //System.out.println("Percent finished: " + Math.ceil((((double)counter / BOARD_SIZE) * 100)));
+        }
+
+    //doesnt consider conditions before placing
+        // int counter = 0;
+        // while (counter < 9) {
+        //     for (int i = 0; i < BOARD_SIZE; i++) {
+        //         int temp = new Random().nextInt(10);
+        //         System.out.println(temp + toString());
+        //         if (conditionsMet(counter, i, temp)) {
+        //             board[counter][i] = temp;
+        //         }
+        //         else {
+        //             i--;
+        //         }
+        //     }
+        //     counter++;
+        // }
+
+    //missing checks for rows and cols
+        // List<Integer> vals = Arrays.asList(1,2,3,4,5,6,7,8,9);
+        // int counter = 0;
+        // while (counter < 40) {
+        //     Collections.shuffle(vals);
+        //     for (int i = 0; i < BOARD_SIZE / 3; i++) {
+        //         for (int j = 0; j < BOARD_SIZE / 3; j++) {
+        //             if (conditionsMet(i, j, vals.get(counter % 9))) {
+        //                 board[i][j] = vals.get(counter % 9);
+        //                 counter++;
+        //             }
+        //             else if (conditionsMet(i+3, j, vals.get(counter % 9))) {
+        //                 board[i+3][j] = vals.get(counter % 9);
+        //                 counter++;
+        //             }
+        //         }
+        //     }
+        //     System.out.println(toString());
+        // }
+        removeNums();
+    }
+    private void removeNums()
+    {
+        for (int i = 0; i < BOARD_SIZE * 5; i++) {
+            int tempRow = new Random().nextInt(9);
+            int tempCol = new Random().nextInt(9);
+            if (board[tempRow][tempCol] != 0) {
+                board[tempRow][tempCol] = 0;
+            }
+            else {
+                i--;
             }
         }
     }
     public boolean conditionsMet(int row, int col, int ans)
     {
-        if (ans > 9 && ans < 0) {
+        if (ans == 0) return true;
+        if (ans > 9 || ans < 0) {
             return false;
         }
     	if (row >= 0 && row < 9 && col >= 0 && col < 9) {
